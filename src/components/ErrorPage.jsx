d31a35163089d43e233aa691b48f5cb1de615b11
JP-GS <juanpgordillo89@gmail.com>
2023-09-23 19:42:27 -0300

    mensaje de error mejorado

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -9,9 +9,8 @@ function ErrorPage() {
             className="text-center font-extrabold text-blue-800 uppercase text-6xl m-20"
         >CRM Clientes</h1>
 
-        <p className="text-center text-gray-800 uppercase mb-5">Ups! Hubo un error</p>
-        <p></p>
-        <p className="text-center font-extrabold text-blue-500 uppercase text-xl m-20">Este es un proyecto de prueba y no tiene una base de datos real</p>
+        <p className="text-center text-gray-800 uppercase mb-5">Ups! Hubo un error!! Aqui deberían listarse los clientes</p>
+        <p className="text-center font-extrabold text-blue-500 uppercase text-xl mb-5">Este es un proyecto de prueba y no tiene una base de datos real</p>
         <p className="text-center">{error.statusText || error.message}</p>
     </div>
   )
